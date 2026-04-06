e61187fab2a455180c8f165e76e1c65250a46e57
Alokananda Y <plantsfortrees@gmail.com>
2026-04-06 13:17:12 +0530

    working with variables

---
+++ devops_path/python/math_with_strings.py
@@ -0,0 +1,11 @@
+sentence_start = "You have "
+sentence_end = " health"
+
+player1_health = "1200"
+player2_health = "1100"
+
+# Don't edit above this line
+
+print(f"You have {player1_health}")
+print(f"You have {player2_health}")
+
---
+++ devops_path/python/variable.py
@@ -0,0 +1,25 @@
+# ?
+player_health = 1000
+
+# Don't touch below this line
+
+print(player_health)
+
+player_health = 1000
+
+# reduce by 100 here
+
+print(player_health - 100)
+
+# and here
+
+print(player_health - 200)
+
+# and here
+
+print(player_health - 300)
+
+# and here
+
+print(player_health - 400)
+
