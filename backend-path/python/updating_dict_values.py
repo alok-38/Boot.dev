59259ed9fa7340c6ce991d259238bbe3f88b4e35
Alokananda Y <plantsfortrees@gmail.com>
2026-04-29 07:19:17 +0530

    Updating Dictionary Values

---
+++ backend-path/python/updating_dict_values.py
@@ -0,0 +1,4 @@
+data = {"a": 1, "b": 2}
+data["a"] = 10
+print(data)
+
---
+++ devops-path/python/assignment/updatating_dictionary_values.py
@@ -0,0 +1,12 @@
+full_names = ["jack bronson", "james mcarty", "jack denver"]
+
+names_dict = {}
+for full_name in full_names:
+    # .split() returns a list of strings
+    # where each string is a single word from the original
+    names = full_name.split()
+    first_name = names[0]
+    last_name = names[1]
+    names_dict[first_name] = last_name
+
+print(names_dict)
